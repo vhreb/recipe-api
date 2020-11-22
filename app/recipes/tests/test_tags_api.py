9c68594a68e45927d09167e16fa0eac72aa779f3
vhreb <vitalii@shipwell.com>
2020-11-22 18:06:42 +0200

    add create tag

--- app/recipes/tests/test_tags_api.py
+++ app/recipes/tests/test_tags_api.py
@@ -51,10 +51,28 @@ class PrivateTagsAPITestCase(UserAPITestMixin, TestCase):
             email='other@gmail.com',
             password='password'
         )
-        tag = Tag.objects.create(user=user2, name='Fruity')
-        self.client.force_authenticate(user2)
+        Tag.objects.create(user=user2, name='Comfort Food')
+        tag = Tag.objects.create(user=self.user, name='Fruity')
         res = self.client.get(self.url)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
+
+    def test_create_tag_successful(self):
+        payload = {
+            'name': 'Test tag',
+        }
+        self.client.post(self.url, payload)
+
+        exists = Tag.objects.filter(
+            user=self.user,
+            name=payload['name'],
+        ).exists()
+        self.assertTrue(exists)
+
+    def test_create_tag_invalid(self):
+        payload = {'name': ''}
+        res = self.client.post(self.url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
