30ec898512492469091e8a584bcffd43103765bc
vhreb <vitalii@shipwell.com>
2020-11-22 16:24:54 +0200

    remove spaces

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,7 +1,7 @@
 import time
 
 from django.db import connections
-from django.db.utils import  OperationalError
+from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 
